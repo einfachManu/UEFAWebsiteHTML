9d11dd374c93c9512b9e76f428ae3160996d4d9b
einfachManu <maschmelone2002@gmail.com>
2025-06-05 20:47:49 +0200

    New initialisation

--- config/config.js
+++
@@ -1,3 +0,0 @@
-export const RECAPTCHA_KEY_PUBLIC = '6LdZLiQrAAAAAGozk6jDs0KOKhgH2kAQ4ZxE-mOZ';
-export const RECAPTCHA_KEY_SECRET = '6LdZLiQrAAAAAOW87kWekFUxEb-cvbQpCVhocsJg';
-export const RECAPTCHA_SCORE      = 0.6;

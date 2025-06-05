17c5dcd02a731e6831bd88dae0a7ef3e60105526
einfachManu <maschmelone2002@gmail.com>
2025-06-05 19:24:27 +0200

    add : completly New initalisation

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $recaptcha_url      = 'https://www.google.com/recaptcha/api/siteverify';
+    $recaptcha_secret   = '6LdZLiQrAAAAAOW87kWekFUxEb-cvbQpCVhocsJg';
+    $recaptcha_response = $_POST['g-recaptcha-response'];
+
+    $recaptcha = file_get_contents(
+        $recaptcha_url
+        . '?secret=' . $recaptcha_secret
+        . '&response=' . $recaptcha_response
+    );
+    $recaptcha = json_decode($recaptcha, true);
+
+    if (
+        $recaptcha['success'] == 1
+        AND $recaptcha['score'] >= 0.5
+        AND $recaptcha['action'] == "login"
+    ) {
+        alert("reCAPTCHA verified successfully!" + "your score: " + $recaptcha['score']);
+    } else {
+        alert("reCAPTCHA verification failed! " + "your score: " + $recaptcha['score']);
+    }
+
+}
